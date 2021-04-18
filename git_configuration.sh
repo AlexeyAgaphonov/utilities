f2dbb16e606b4d017b2a51f90262797a725b594a
Alexey <alexeyagaphonov@gmail.com>
2021-04-18 18:22:40 +0300

    alias.cm fix

--- git_configuration.sh
+++ git_configuration.sh
@@ -2,7 +2,7 @@
 
 git config --global alias.up "pull --rebase"
 git config --global alias.pu "push"
-git config --global alias.cm "config -m"
+git config --global alias.cm "commit -m"
 git config --global alias.st "status"
 git config --global alias.co "checkout"
 git config --global alias.smu "submodule update --init --recursive"
