35b30d2c5fd18532392ce8e9cbae8d6952c745f3
Alexey <alexeyagaphonov@gmail.com>
2021-04-18 18:22:21 +0300

    конфигурация alias'ов гита

---
+++ git_configuration.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+git config --global alias.up "pull --rebase"
+git config --global alias.pu "push"
+git config --global alias.cm "config -m"
+git config --global alias.st "status"
+git config --global alias.co "checkout"
+git config --global alias.smu "submodule update --init --recursive"
+git config --global alias.ca "commit --amend"
